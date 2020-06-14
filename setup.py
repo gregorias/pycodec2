21e4159074b23bf544ef8eee0df059c196eb1bb3
Grzegorz Milka <grzegorzmilka@gmail.com>
2020-06-14 19:54:40 +0200

    Update setup.py
    This commit adjusts the setup.py as recommended by
    [SO](https://stackoverflow.com/a/4515279/915552)

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-import numpy as np 
+import numpy as np
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 
 ext_modules = [
     Extension("pycodec2",
@@ -19,15 +19,13 @@ setup(
   name = "pycodec2",
   packages = ['pycodec2'],
   version = VERSION,
-  description = 'Cython wrapper for codec2',
+  description = 'A Cython wrapper for codec2',
   author = 'Grzegorz Milka',
   author_email = 'grzegorzmilka@gmail.com',
   url = 'https://github.com/gregorias/pycodec2',
-  download_url = 'https://github.com/gregorias/pycodec2/tarball/' + VERSION,
   keywords = ['codec2', 'audio', 'voice'],
   classifiers = ['Topic :: Multimedia :: Sound/Audio :: Speech',
                  'License :: OSI Approved :: BSD License',
-                 'Programming Language :: Python :: 2',
                  'Programming Language :: Python :: 3',
                  'Development Status :: 5 - Production/Stable',
                  ],
